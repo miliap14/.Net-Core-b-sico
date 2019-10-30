f3cf8cbbcd85d0058908d1a4f8b3eb8ed9501004
Emiliano Piovano <emi.piovano@gmail.com>
2019-10-30 10:42:22 -0300

    Trabajando en colecciones

--- Program.cs
+++ Program.cs
@@ -12,12 +12,21 @@ namespace etapa1
             var escuela = new Escuela("Sancor Academy", 2019, TiposEscuela.Primaria);
             
             escuela.Cursos = new List<Curso>{
+                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana},
+                new Curso(){ Nombre = "201", Jornada = TiposJornada.Mañana},
+                new Curso(){ Nombre = "301", Jornada = TiposJornada.Mañana},
+            };
+            
+            escuela.Cursos.Add( new Curso (){ Nombre = "102", Jornada = TiposJornada.Tarde });
+            escuela.Cursos.Add( new Curso (){ Nombre = "202", Jornada = TiposJornada.Tarde });
+
+            var otraColeccion = new List<Curso>{
                 new Curso(){ Nombre = "101"},
                 new Curso(){ Nombre = "201"},
                 new Curso(){ Nombre = "301"},
             };
-            
-            
+
+            escuela.Cursos.AddRange();
         
 
             Console.WriteLine(escuela);
